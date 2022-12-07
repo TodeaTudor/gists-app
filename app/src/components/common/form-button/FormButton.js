2ad707d354d55a5d4aa4306cdb74aae01e592f55
TudorTodea <todeastudor@gmail.com>
2022-12-07 19:56:51 +0200

    removed unnecessary fragment

--- app/src/components/common/form-button/FormButton.js
+++ app/src/components/common/form-button/FormButton.js
@@ -3,9 +3,7 @@ import styles from "./FormButton.module.css";
 const FormButton = (props) => {
 
   return (
-    <>
-      <button type={props.type} className={styles.button} onClick={props.onClick}>{props.children}</button>
-    </>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>{props.children}</button>
   );
 }
 
